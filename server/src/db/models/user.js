8c8231db6a1d2b1dec5d2375d401079f3b20eabb
Kristina <kono.kristina2016@yandex.ru>
2025-11-06 09:14:57 +0300

    val

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,25 +1,97 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models) {}
+
+    static validatePassword(password) {
+      // валидация пароля
+      const hasUpperCase = /[A-Z]/;
+      const hasLowerCase = /[a-z]/;
+      const hasDigits = /[0-9]/;
+      const hasSpecialCharacters = /[!@#$%^&*(),.:"{}|<>]/;
+      const isValidLength = password.length >= 8;
+
+      if (
+        !hasUpperCase.test(password) ||
+        !hasLowerCase.test(password) ||
+        !hasDigits.test(password) ||
+        !hasSpecialCharacters.test(password) ||
+        !isValidLength
+      ) {
+        return false;
+      }
+      return true;
+    }
+
+    static validateSignUpData(data) {
+      // валидация введенных данных при регистрации
+      const { userName, email, password } = data;
+
+      if (
+        !userName ||
+        typeof userName !== "string" ||
+        userName.trim().length === 0
+      ) {
+        return { isValid: false, error: "Неверное имя пользователя" };
+      }
+
+      if (
+        !email ||
+        typeof email !== "string" ||
+        email.trim().length === 0 ||
+        !this.validateEmail(email)
+      ) {
+        return { isValid: false, error: "Неверный адрес электронной почты" };
+      }
+
+      if (
+        !password ||
+        typeof password !== "string" ||
+        password.trim().length === 0 ||
+        !this.validatePassword(password)
+      ) {
+        return {
+          isValid: false,
+          error: "Пароль не соответствует критериям валидации",
+        };
+      }
+
+      return { isValid: true, error: null };
+    }
+
+    static validateSignInData(data) {
+      // валидация введенных данных если пользователь уже зарегистрирован
+      const { email, password } = data;
+
+      if (!email || typeof email !== "string" || email.trim().length === 0) {
+        return { isValid: false, error: "Неверный адрес электронной почты" };
+      }
+
+      if (
+        !password ||
+        typeof password !== "string" ||
+        password.trim().length === 0
+      ) {
+        return {
+          isValid: false,
+          error: "Пароль не соответствует критериям валидации",
+        };
+      }
+
+      return { isValid: true, error: null };
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
