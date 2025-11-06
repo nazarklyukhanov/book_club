3472ff21aebb4b38da4a7a7b841480192ab2997c
Anton Makarov <proverka.k1@yandex.ru>
2025-11-06 21:08:42 +0500

    onebook, mainpage

--- client/src/pages/OneBookPage/OneBookPage.jsx
+++ client/src/pages/OneBookPage/OneBookPage.jsx
@@ -1,9 +1,13 @@
 import "./OneBookPage.css";
 
-export default function OneBookPage() {
-    
+export default function OneBookPage({book}) {
     return(
-
-        <h1>Страница одной книги</h1>
+<div> 
+<p>{book.name}</p>
+<p>{book.autor}</p>
+<p>{book.cover}</p>
+<p>{book.comment_of_user}</p>
+<p>{book.user_id}</p>
+</div>
     )
 };
