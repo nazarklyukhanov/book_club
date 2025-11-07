b9cc60cfc2c040c970f301ac111127e87cb8efc0
nazarklyukhanov <nklyukhanov@gmail.com>
2025-11-07 14:00:32 +0300

    main page

--- client/src/pages/OneBookPage/OneBookPage.jsx
+++ client/src/pages/OneBookPage/OneBookPage.jsx
@@ -1,35 +1,30 @@
-
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import BookApi from '../../entities/BookApi';
-import Raiting from '../../components/Raiting/Raiting';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import BookApi from "../../entities/BookApi";
+import Raiting from "../../components/Raiting/Raiting";
 import styles from "./OneBookPage.module.css";
 
-
 export default function OneBookPage() {
+  const [book, setBook] = useState({});
 
-const [book, setBook] = useState({})
-
-const { id } = useParams()
+  const { id } = useParams();
 
   useEffect(() => {
-async function fetchOneBook() {
-    const data = await BookApi.getOneBook(id);
-    setBook(data.data);
-  }
+    async function fetchOneBook() {
+      const data = await BookApi.getOneBook(id);
+      setBook(data.data);
+    }
     fetchOneBook();
-    
   }, []);
 
-
   return (
-   <div> 
-<p>{book.name}</p>
-<p>{book.autor}</p>
-<p>{book.cover}</p>
-<p>{book.comment_of_user}</p>
+    <div>
+      <p>{book.name}</p>
+      <p>{book.autor}</p>
+      <p>{book.cover}</p>
+      <p>{book.comment_of_user}</p>
       <p>{book.user_id}</p>
       <Review />
-</div>
-  )
+    </div>
+  );
 }
