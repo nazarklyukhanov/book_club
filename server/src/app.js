a154eac2a5d2614a006a4df0a283dc0c3f0d18c6
Rakiiia <Rakiiia@mail.ru>
2025-11-07 15:15:59 +0300

    sd

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const apiRouter = require("./routes/api.router");
 const serverConfig = require("./config/server.config");
 // const process = require('process')
-const aiRouter = require('./routes/ai.router')
 const cors = require('cors'); // добавил
 
 const app = express();
@@ -13,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 serverConfig(app);
 
 app.use("/api", apiRouter);
-app.use('/api/ai', aiRouter); // вот здесь вопрос правильно или нет или вдругое место ставить)
+
 
 
 app.listen(PORT, () => {
